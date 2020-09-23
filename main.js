2b5a28000362cfab0610679d30f17e79c0157a9f
Bruno Thomas <bthomas@icij.org>
2020-09-23 17:02:07 +0000

    integrate the pipeline for document tabs

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 import Posts from './Posts'
 
-document.addEventListener('datashare:ready', async ({ detail }) => {
-  detail.core.registerHook({
-    target: 'document.content:after',
-    definition: Posts
-  })
-}, false)
+document.addEventListener('datashare:ready', async ({ detail: { core } }) => {
+    // We register a post-pipeline function for the `document-view-tabs` category
+    core.registerPipeline({
+      name: 'document-view-tabs-test',
+      category: 'document-view-tabs:post',
+      // The function that is applied to the tabs list
+      type (tabs, document) {
+        tabs.push({
+          name: 'comments-tab',
+          label: 'Comments',
+          icon: 'align-left',
+          props: { document },
+          component: Posts
+        })
+        return tabs
+      }
+    })
+  }, true)
