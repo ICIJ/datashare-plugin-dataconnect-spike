dccf681cceec5a1a644f160a7433d179139acf55
Bruno Thomas <bthomas@icij.org>
2020-10-07 09:38:20 +0000

    adds a new comment button

--- Posts.vue
+++ Posts.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="posts p-3">
+    <div class="posts__actions">
+        <div class="posts__actions__form-group">
+          <textarea class="posts__actions__form-group-textarea form-control rounded-0" v-model="comment" placeholder="new comment"></textarea>
+          <button class="posts__actions__form-group__create btn btn-primary" @click="createComment()">Create</button>
+        </div>
+      </div>
     <div v-for="post in posts" :key="post.id" class="posts__post">
       <div class="posts__post__header row">
         <div class="posts__post__header__author col-6 font-weight-bold">
@@ -21,7 +27,8 @@
     name: 'Posts',
     data () {
       return {
-        posts: []
+        posts: [],
+        comment: ""
       }
     },
     async mounted () {
@@ -29,11 +36,26 @@
       let index = pathFragments[2]
       let docId = pathFragments[3]
       const url = new URL(`/comments/${index}/${docId}/all`, `${window.location.protocol}//${window.location.host}`)
-      axios.get(url.href)
-        .then(response => {
-          this.$set(this, 'posts', response.data.post_stream.posts)
-        })
-        .catch(e => console.log('error', e))
+      try {
+        const resp = await axios.get(url.href)
+        this.$set(this, 'posts', resp.data.post_stream.posts)
+      } catch (e ) {
+        console.log('error', e)
+      }
+    },
+    methods: {
+      async createComment () {
+        let pathFragments = window.location.hash.split("/"); // #/d/index/docid/routing
+        let index = pathFragments[2]
+        let docId = pathFragments[3]
+        try {
+          await axios.put(
+              new URL(`/comments/${index}/${docId}`, `${window.location.protocol}//${window.location.host}`).href,
+              {"content": this.comment})
+        } catch (e )  {
+          console.log('error', e)
+        }
+      }
     }
   }
 </script>
